5c7488a58ffdecffabfa72e2143603192a0dfead
Chris Schotman <chris@draad.nl>
2025-03-05 15:15:33 +0100

    Compiled block

--- build/draad-adreszoeker/index.asset.php
+++ build/draad-adreszoeker/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '3d9a90091628c1870cba');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '16bf0d5a130d0f4d2a29');
