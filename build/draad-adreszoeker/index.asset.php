b148f80a15a826056cab74ecc171a2b98af32da4
chrisschotman <chris@draad.nl>
2026-03-26 08:08:04 +0100

    refactor: remove legacy draad-adreszoeker block
    Removed the legacy ACF-based block and its source files, build artifacts,
    and PHP templates. The native blocks (formulier and output) replace it.

--- build/draad-adreszoeker/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '83d971322e45885323fb');
--- build/draad-adreszoeker/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'bef2c2da8dbd39f35172');
