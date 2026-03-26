cc9fdb5aa72f1cd80384c9c52c4e4c098d79d33b
chrisschotman <chris@draad.nl>
2026-03-26 19:24:10 +0100

    fix(tokens): scope Utrecht component CSS to .draad-adreszoeker
    Wraps imported Utrecht component styles inside the .draad-adreszoeker
    selector to prevent style leakage to other page elements.

--- build/draad-adreszoeker-output/index.asset.php
+++ build/draad-adreszoeker-output/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'edafd24de3584c5d95df');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f4f1a0c8df21796ff021');
