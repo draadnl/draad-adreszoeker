39c5b789f0caf6a1f9a95ddf5368ff536cf68c1a
chrisschotman <chris@draad.nl>
2026-03-26 08:04:49 +0100

    fix(tokens): scope design system tokens to .draad-adreszoeker
    Scoped all .utrecht-theme and .denhaag-theme token selectors to
    .draad-adreszoeker to prevent overriding site-wide button and component
    styles on denhaag.nl. Removed redundant unscoped duplicate .denhaag-theme block.

--- build/draad-adreszoeker-output/index.asset.php
+++ build/draad-adreszoeker-output/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f4aab12901ef8f77d55a');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'edafd24de3584c5d95df');
