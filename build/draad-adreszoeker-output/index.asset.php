fe5374a3e9a7433d71b5038888b9768665779766
chrisschotman <chris@draad.nl>
2026-04-16 10:45:06 +0200

    style(button): align to Den Haag design system
    Add _button.scss to correct focus ring outline-offset to 2px as
    specified in the Figma design (token sets 0px via --denhaag-focus-offset).
    Consolidate identical .denhaag-button layout rules from both block
    stylesheets into the shared partial to remove duplication.
    
    Green primary/secondary colors and dashed ocher focus ring are already
    configured via --denhaag-button-* tokens in tokens.scss under
    .draad-adreszoeker.denhaag-theme.

--- build/draad-adreszoeker-output/index.asset.php
+++ build/draad-adreszoeker-output/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '114264dfb3cfb914358d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '85c762a1f79e516ac241');
