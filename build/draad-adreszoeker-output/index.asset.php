1dc31453863f68cd59683b2a353d2abc31d02615
chrisschotman <chris@draad.nl>
2026-04-16 11:11:25 +0200

    fix(button): upsize to Figma large spec (48px)
    Button renders without size="large" prop so it defaults to medium
    tokens (43px). Override --denhaag-button-medium-size-padding-block/inline
    and --denhaag-button-font-size to large equivalents. Add line-height
    override at higher specificity (0,3,0) to beat the component's
    hardcoded line-height: 1.5 injected at runtime.

--- build/draad-adreszoeker-output/index.asset.php
+++ build/draad-adreszoeker-output/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '85c762a1f79e516ac241');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '67501dc19175e32ff1f4');
